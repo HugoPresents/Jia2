159f4258e57f78311465c06567e2abda9f8cd2e2
Tuzki <rabbitzhang52@gmail.com>
2012-08-09 22:06:29 +0800

    change config

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'test';
-$db['default']['password'] = 'test';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'rabbit';
 $db['default']['database'] = 'jia2';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
