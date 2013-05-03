6dd7296cc6c3860d9611c8453e116cf2a0bdb42e
Rabbit52 <rabbitzhang52@gmail.com>
2013-05-03 11:27:44 +0800

    delete data directory from repository

--- application/views/includes/css_view.php
+++
@@ -1,9 +0,0 @@
-<? if(isset($css)): ?>
-	<? if(is_array($css)): ?>
-		<? foreach($css as $value): ?>
-			<link rel="stylesheet" type="text/css" href="<?=base_url('resource/css/' . $value) ?>" />
-		<? endforeach; ?>
-	<? else: ?>
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resource/css/' . $css) ?>" />
-	<? endif ?>
-<? endif ?>
--- application/views/static_view.php
+++
@@ -1,6 +0,0 @@
-<div id="static">
-	<p><?=$message ?></p>
-	<? if($url): ?>
-	<p><?=anchor($url, '返回') ?></p>
-	<? endif ?>
-</div>
