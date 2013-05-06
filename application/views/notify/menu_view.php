1235fabd948af750725a1560136c2d746563c58d
Rabbit52 <rabbitzhang52@gmail.com>
2013-05-06 22:51:33 +0800

    notify view tab

--- application/views/notify/menu_view.php
+++ application/views/notify/menu_view.php
@@ -3,13 +3,13 @@
         <div class="notify">
             <div id="notify_menu" class="search_item">
                 <ul class="nav nav-tabs">
-                    <li id="notify_letter" <?=type == 'letter' ? ' class="active"' :''?>>
+                    <li id="notify_letter" <?=$notify == 'letter' ? ' class="active"' :''?>>
                         <a href="<?= site_url('notify?type=letter') ?>">站内信</a>
                     </li>
-                    <li id="notify_request" <?=type == 'request' ? ' class="active"' :''?>>
+                    <li id="notify_request" <?=$notify == 'request' ? ' class="active"' :''?>>
                         <a href="<?= site_url('notify?type=request') ?>">请求</a>
                     </li>
-                    <li id="notify_message" <?=type == 'message' ? ' class="active"' :''?>>
+                    <li id="notify_message" <?=$notify == 'message' ? ' class="active"' :''?>>
                         <a href="<?= site_url('notify?type=message') ?>">消息</a>
                     </li>
                 </ul>
--- application/views/notify_view.php
+++ application/views/notify_view.php
@@ -1,5 +1,2 @@
 <? $this->load->view('notify/menu_view') ?>
-<script type="text/javascript">
-	NOTIFY_NOW = "<?=$notify ?>";
-</script>
 <? $this->load->view('notify/' . $notify . '_view') ?>
