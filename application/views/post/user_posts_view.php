542c0ae4d0c3bc28e95b322ccf7ce20e602161ba
tiramisu101111@gmail.com <liz@jumei.com>
2013-05-06 11:02:31 +0800

    comment & footer

--- application/views/post/user_posts_view.php
+++ application/views/post/user_posts_view.php
@@ -93,7 +93,7 @@
       <!-- 评论等toolbar-->
       <div class="f_summary clearfix">
         <div class="fl"><span><?=time2duration($post['time'])?></span> 来自 <span>成都信息工程学院</span></div>
-          <div class="fr"><a href="javascript:;" class="CommetBtn">评论</a></div>
+          <div class="fr"><a href="javascript:void(0);" class="CommetBtn">评论</a></div>
       </div>
       <div class="repeat">
         <span class="arr">
