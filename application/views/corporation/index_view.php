06a4b3401422fc5733d44f9d1a242d32db417907
tiramisu101111@gmail.com <liz@jumei.com>
2013-05-07 13:26:15 +0800

    社团 tab 切换 & 社团 setting

--- application/views/corporation/index_view.php
+++ application/views/corporation/index_view.php
@@ -7,14 +7,14 @@
         <?
         $first = FALSE;
         if($m_corporations): ?>
-        <a title="我管理的社团" href="javascript:void(0);" id="" class="switch<?= $first ? '':' first'?> selected btn">我管理的社团</a>
+        <a title="我管理的社团" href="#manage" id="" class="switch<?= $first ? '':' first'?> selected btn"  data-toggle="tab">我管理的社团</a>
         <? $first = TRUE;endif ?>
         <? if($j_corporations): 
         ?>
-        <a title="我参加的社团" href="javascript:void(0);" id="" class="switch<?= $first ? '':' first'?> btn">我参加的社团</a>
+        <a title="我参加的社团" href="#join" id="" class="switch<?= $first ? '':' first'?> btn"  data-toggle="tab">我参加的社团</a>
         <? $first = TRUE;endif ?>
         <? if($f_corporations): ?>
-        <a title="我关注的社团" href="javascript:void(0);" id="" class="switch<?= $first ? '':' first'?> btn">我关注的社团</a>
+        <a title="我关注的社团" href="#atten" id="" class="switch<?= $first ? '':' first'?> btn"  data-toggle="tab">我关注的社团</a>
         <? $first = TRUE;endif ?>
         </div>
         <div class="feed_op">
@@ -22,7 +22,8 @@
             <a href="/search/?target=corporation" target="_blank" class="btnDefault btnBlue" >发现社团</a>
         </div>
     </div>
-    <ul class="asso_mg">
+    <div class="tab-content">
+    <ul class="asso_mg tab-pane active" id="manage">
         <? if($m_corporations): ?>
         <? foreach($m_corporations as $corporation): ?>
         <li class="asso_w clearfix">
@@ -42,6 +43,7 @@
         </li>
         <? endforeach ?>
         <? endif ?>
+    </ul><ul class="asso_mg tab-pane" id="join">
         <? if($j_corporations): ?>
         <? foreach($j_corporations as $corporation): ?>
         <li class="asso_w clearfix">
@@ -61,7 +63,8 @@
         </li>
         <? endforeach ?>
         <? endif ?>
-        <? if($f_corporations): ?>
+    </ul><ul class="asso_mg tab-pane" id="atten">
+    <? if($f_corporations): ?>
         <? foreach($f_corporations as $corporation): ?>
         <li class="asso_w clearfix">
             <div class="img_100 fl">
@@ -81,6 +84,7 @@
         <? endforeach ?>
         <? endif ?>
     </ul>
+    </div>
     <h3>全校社团</h3>
     <? if($message): ?>
     <?=$message?>
@@ -88,22 +92,39 @@
     <ul class="thumbnails">
         <? if($school_corporations): ?>
         <? foreach($school_corporations as $corporation): ?>
-        <li class="asso_w clearfix">
-            <div class="img_100 fl">
-                <a href="/corporation/profile/<?=$corporation['id']?>" class="thumbnail">
-                    <img src="<?=avatar_url($corporation['avatar'], 'corporation', 'big')?>" />
-                </a>
-            </div>
-            <div class="asso_brief fl">
-                <h4 class="asso_name"><a href="/corporation/profile/<?=$corporation['id']?>" class="a3"><?=$corporation['name']?></a></h4>
-                <p class="c9">社长：<a href="/personal/profile/<?=$corporation['user'][0]['id']?>"><?=$corporation['user'][0]['name']?></a></p>
-                <p class="c6"><?=$corporation['comment']?></p>
-                <div class="asso_handle">
-                    <a type="button" href="/corporation/setting/<?=$corporation['id']?>" class="btnDefault btnBlue" >管理社团</a>
-                </div>
-            </div>
-        </li>
+<!--        <li class="asso_w clearfix">-->
+<!--            <div class="img_100 fl">-->
+<!--                <a href="/corporation/profile/--><?//=$corporation['id']?><!--" class="thumbnail">-->
+<!--                    <img src="--><?//=avatar_url($corporation['avatar'], 'corporation', 'big')?><!--" />-->
+<!--                </a>-->
+<!--            </div>-->
+<!--            <div class="asso_brief fl">-->
+<!--                <h4 class="asso_name"><a href="/corporation/profile/--><?//=$corporation['id']?><!--" class="a3">--><?//=$corporation['name']?><!--</a></h4>-->
+<!--                <p class="c9">社长：<a href="/personal/profile/--><?//=$corporation['user'][0]['id']?><!--">--><?//=$corporation['user'][0]['name']?><!--</a></p>-->
+<!--                <p class="c6">--><?//=$corporation['comment']?><!--</p>-->
+<!--                <div class="asso_handle">-->
+<!--                    <a type="button" href="/corporation/setting/--><?//=$corporation['id']?><!--" class="btnDefault btnBlue" >管理社团</a>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </li>-->
+                <li class="span3">
+                    <a class="thumbnail"  href="/corporation/profile/<?=$corporation['id']?>">
+                        <img src="<?=avatar_url($corporation['avatar'], 'corporation', 'big')?>">
+                        <p class="asso_tit"><?=$corporation['name']?></p>
+                    </a>
+                </li>
         <? endforeach ?>
         <? endif ?>
     </ul>
 </div>
+<script type="text/javascript" src="<?= base_url('resource/js/new/tab.js') ?>"></script>
+<script>
+    $(function () {
+        $('.switch').click(function (e) {
+            $(".switch").removeClass("selected");
+            $(this).addClass("selected");
+            e.preventDefault();
+            $(this).tab('show');
+        });
+    });
+</script>
