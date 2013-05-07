4385d8a28e9225223133d9ea9bf112029f05b2e6
tiramisu101111@gmail.com <liz@jumei.com>
2013-05-07 19:01:28 +0800

    404 page

--- application/views/static_view.php
+++ application/views/static_view.php
@@ -1,7 +1,12 @@
-<div class="mainContainer">
-    <div class="container whiteBg">
+<div class="mainContainer sta_main">
+    <div class="container">
         <div id="static">
-            <p><?=$message ?></p>
+            <p>
+                <?=$message ?>  ╮(╯_╰)╭
+                <br>
+                <a href="javascript:history.go(-1);">返回上一页</a>
+            </p>
+
             <? if ($url): ?>
                 <p><?=anchor($url, '返回') ?></p>
             <? endif ?>
